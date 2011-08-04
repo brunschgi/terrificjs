49742c54eba5240d49247e578c052f61af8f1e49
Hendrik Grahl <hendrik.grahl@namics.com>
2011-08-04 15:19:07 +0200

    build and test

--- src/core/Tc.Application.js
+++ src/core/Tc.Application.js
@@ -94,22 +94,22 @@
 
                 /**
                  * A module can have three types of classes:
-                 * .mod 
+                 * - .mod 
                  *      Indicates that it is a base module (default 
                  *      -> no javascript need to be involved). Must occur
                  *      excactly once.
                  *
-                 * .mod<moduleName> (e.g. .modBasic) 
+                 * - .mod<moduleName> (e.g. .modBasic) 
                  *      Indicates that it is a module of type basic, which is
                  *      derived from the base module. It can occur at most
                  *      once.
-                 * .skin<moduleName><skinName> (e.g. .skinBasicSubmarine) 
+                 * - .skin<moduleName><skinName> (e.g. .skinBasicSubmarine) 
                  *      Indicates that the module basic has the submarine skin.
                  *      It will be decorated by the skin js (if existing). It
                  *      can occur arbitrarily.
                  *
                  * Additionally, a module can have one type of data attributes:
-                 * data-connectors
+                 * - data-connectors
                  *      A comma-separated value containing the connector ids,
                  *      the schema of a connector id is: 
                  *      <connectorName><connectorId><connectorRole>
