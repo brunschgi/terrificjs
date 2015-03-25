47003340f25955ee2fa45e524d443228a10671ab
Remo Brunschwiler <remo@terrifically.org>
2015-03-25 17:33:19 +0100

    #43 remove connector stuff

---
+++ src/Tc.Connector.js
@@ -0,0 +1,21 @@
+/**
+ * Responsible for inter-module communication.
+ * Connectors are inspired by Node.js EventEmitter
+ *
+ * @author Remo Brunschwiler
+ * @namespace Tc
+ * @class Connector
+ */
+Tc.Connector = Class.extend({
+
+    /**
+     * Initializes the Connector.
+     *
+     * @method init
+     * @constructor
+     */
+    init: function () {
+
+    }
+});
+
