c29e9c39373070ff625c28e3ffb3c82573b24101
Jan Biasi <jan.biasi@namics.com>
2017-10-24 14:07:33 +0200

    feat(typing): Adding flow-typed for flow projects

---
+++ flow-typed/terrific.js
@@ -0,0 +1,83 @@
+// @flow
+
+/**
+ * Basic typings for flow environements
+ * @author Jan Biasi <biasijan@gmail.com>
+ * @version 1.0
+ * @module T
+ */
+declare module T {
+    declare class Application {
+        _ctx: Element;
+        _sandbox: Sandbox;
+        _config: any;
+        _modules: any;
+        _id: number;
+
+        constructor(ctx?: Element, config?: any): Application;
+
+        registerModules(ctx?: Element): Module[];
+        unregisterModules(modules?: any): void;
+        start(modules?: any): Promise<void>;
+        stop(modules?: any): void;
+        registerModule(ctx: Element, mod: string, namespace?: any): Module;
+        registerModule(ctx: Element, mod: string, decorators?: string | string[]): Module;
+        registerModule(ctx: Element, mod: string, decorators?: string | string[], namespace?: any): Module;
+        getModuleById(id: number): Module;
+    }
+
+    declare class EventEmitter {
+        _listeners: any;
+        _sandbox: Sandbox;
+        _connected: boolean;
+
+        constructor(sandbox: Sandbox): EventEmitter;
+
+        on(event: string, listener: (...allArguments: any[]) => void): EventEmitter;
+        addListener(event: string, listener: (...allArguments: any[]) => void): EventEmitter;
+        once(event: string, listener: (...allArguments: any[]) => void): EventEmitter;
+        off(): EventEmitter;
+        off(event: string): EventEmitter;
+        off(event: string, listener: Function): EventEmitter;
+        removeListener(): EventEmitter;
+        removeListener(event: string): EventEmitter;
+        removeListener(event: string, listener: Function): EventEmitter;
+        removeAllListeners(event: string): EventEmitter;
+        emit(...allArguments: any[]): EventEmitter;
+        handle(event: string, ...allArguments: any[]): EventEmitter;
+        listeners(event: string): ((...allArguments: any[]) => void)[];
+        hasListeners(event: string): boolean;
+        connect(): EventEmitter;
+        disconnect(): EventEmitter;
+    }
+
+    declare class Sandbox {
+        _application: Application;
+        _eventEmitters: EventEmitter[];
+
+        constructor(application: Application): Sandbox;
+
+        addModules(ctx: Element): Module[];
+        removeModules(modules: Element | Module[]): Sandbox;
+        getModuleById(id: number): Module;
+        getConfig(): any;
+        getConfigParam(name: string): any;
+        addEventEmitter(eventEmitter: EventEmitter): Sandbox;
+        removeEventEmitter(eventEmitter: EventEmitter): Sandbox;
+        dispatch(...allArguments: any[]): Sandbox;
+    }
+
+    declare class Module {
+        _ctx: Element;
+        _sandbox: Sandbox;
+        _events: EventEmitter;
+
+        constructor(ctx: Element, sandbox: Sandbox): Module;
+
+        start(resolve: (value?: any) => void, reject: (error?: any) => void): void;
+        stop(): void;
+    }
+
+    declare export function createModule(spec: any): Function;
+    declare export function createDecorator(spec: any): Function;
+}
